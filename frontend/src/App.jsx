31a22bffc313e9513d178c88e45c9969cb0f9e9c
abhigit-saha <abhijitsaha1006@gmail.com>
2024-10-29 13:45:27 +0530

    Resynced with the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './home/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <Routes>
-     <Route path="/" element={<Home/>}/>
-     <Route path="/login" element={<Login />}/>
-     <Route path="/Sign Up" element={<Signup />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Sign Up" element={<Signup />} />
     </Routes>
   );
 }
